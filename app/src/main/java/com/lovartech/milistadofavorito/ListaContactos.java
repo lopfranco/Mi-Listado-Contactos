26d052402c5e39ab2cef24e810f9ae2a076f90a6
Francisco Lopez <lopez.fko@icloud.com>
2019-03-25 22:00:27 -0600

    Listado de Contactos

---
+++ app/src/main/java/com/lovartech/milistadofavorito/ListaContactos.java
@@ -0,0 +1,19 @@
+package com.lovartech.milistadofavorito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListaContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contactos);
+
+
+
+    }
+}
